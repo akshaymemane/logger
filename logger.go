ca359d8217222af333d3669911b0917858420858
Akshay Memane <akshaymemane29@gmail.com>
2024-03-25 00:24:59 +0530

    filename path formatting

--- logger.go
+++ logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 
@@ -49,7 +50,7 @@ func New() *Logger {
 	}
 
 	return &Logger{
-		Logger:        log.New(file, "Logger: ", log.Ldate|log.Ltime|log.Lshortfile),
+		Logger:        log.New(file, "Logger: ", log.Ldate|log.Ltime),
 		level:         logLevel,
 		logFormatFlag: logFormatFlag,
 	}
@@ -81,19 +82,64 @@ func (l *Logger) Error(msg string, args ...interface{}) {
 
 func (l *Logger) printLog(level, formattedMsg string, args ...interface{}) {
 	_, file, line, _ := runtime.Caller(2)
-	file = trimGOPATH(file)
-
+	file = transformFilePath(file)
 	msg := fmt.Sprintf(formattedMsg, args...)
 
-	l.Printf("%s %s:%d %s", level, file, line, msg)
+	l.Printf("%s:%d %s %s", file, line, level, msg)
 }
 
-func trimGOPATH(path string) string {
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		return path
+// func trimGOPATH(path string) string {
+// 	gopath := os.Getenv("GOPATH")
+// 	if gopath == "" {
+// 		return path
+// 	}
+// 	return strings.TrimPrefix(path, gopath+"/src/")
+// }
+
+func transformFilePath(file string) string {
+	// Get the path of the directory containing the main package file
+	rootDir := getProjectRootDir()
+	if rootDir == "" {
+		// Return original file path if unable to determine project root directory
+		return file
 	}
-	return strings.TrimPrefix(path, gopath+"/src/")
+
+	// Compute the relative path between the project root directory and the file
+	relPath, err := filepath.Rel(rootDir, file)
+	if err != nil {
+		// Handle error if unable to compute relative path
+		return file // Return original file path
+	}
+
+	return relPath
+}
+
+func getProjectRootDir() string {
+	// Get the path to the directory containing the main package file
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		return ""
+	}
+
+	// Traverse up the directory hierarchy until finding a known file (e.g., main.go)
+	for {
+		if isRootDir(filename) {
+			return filename
+		}
+		parentDir := filepath.Dir(filename)
+		if parentDir == filename {
+			return ""
+		}
+		filename = parentDir
+	}
+}
+
+func isRootDir(dir string) bool {
+	// Add any conditions to identify the root directory here
+	// For example, you might check for the presence of a specific file
+	// like "main.go" or a specific directory structure.
+	_, err := os.Stat(filepath.Join(dir, "main.go"))
+	return err == nil
 }
 
 func LogLevelFromString(levelStr string) LogLevel {
